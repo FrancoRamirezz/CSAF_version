b3c08f9df824a3faf3998dee5a638f36010ff3ec
Francisco Ramirez <96508706+FrancoRamirezz@users.noreply.github.com>
2023-09-14 00:14:28 -0700

    Add files via upload

---
+++ src/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+const About = () =>{
+
+return(
+<div>
+    <h1>About page</h1>
+    <p>This Project is to demonstrate the importance of security measures when visiting an ecommerce website. This project consisted of
+different specialties, such software engineering, cybersecurity, and data analytics. This project could not have been done without the help
+from the CSAF lead Josh, the software team that consisted of Franco, Meghan, Ally, the data analytics team that consisted of Abhishek, and cyber/cloud
+team that consisted of Josh, Palmer, and Salvador
+
+    </p>
+
+</div>
+
+
+
+);
+
+};
+export default About;
