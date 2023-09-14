e25fb59efb58074fd625c542d2b985aeb2076959
Francisco Ramirez <96508706+FrancoRamirezz@users.noreply.github.com>
2023-09-13 23:58:35 -0700

    Update Navbar.js

---
+++ src/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+function Navabr(){
+return < nav className="nav">
+<img src = " "/>
+<a href = '/' className="site-title" >
+    Homepage
+</a>
+<ul>
+    <li>
+        <a href= "/shoppingcart" >ShoppingCart </a></li>
+        
+    <li>
+    <a href= "/contact"> Contact</a>    
+    </li>
+    <li> 
+    <a href = "/About"> About</a>
+    </li>
+</ul>
+
+
+
+</nav>
+
+
+}
+export default Navabr;
