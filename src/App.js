ccdd8b4d3ef581f9b09ae1c223380c5afb4129ea
Francisco Ramirez <96508706+FrancoRamirezz@users.noreply.github.com>
2023-09-14 00:04:13 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import Homepage from './Homepage';
+import About from './About';
+import Navabr from './Navbar';
+import Contact from "./Contact";
+import ShoppingCart from "./shoppingcart";
+import { Routes, Route, Link, BrowserRouter, NavLink, createBrowserRouter, createRoutesFromElements} from 'react-router-dom';
+import { Navbar } from 'react-bootstrap';
+const logo_cf = "MISSA Ecomerce Project "
 
+//Call the other componets using from then the names of the componets
 function App() {
   return (
+    //Make a router component to connect the home to the navbar and then the shopping cart
+    <BrowserRouter>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{logo_cf}</h1>
+        <img src   className="App-logo" alt="logo" />
+        <nav className='App-link'>
+          <Link to ="/Homepage">Click This Link</Link>
+        
+        </nav>
+        
+        <main>
+        <Routes>
+           <Route path = "/Homepage" element = {<Homepage/>}>
+          <Route path = 'Navabr' element = {<Navbar/>}>
+          <Route path = "About" element = {<About/>}> 
+
+          </Route>
+          </Route>
+          </Route>
+          </Routes>
+
+        </main>
+  
+      
       </header>
     </div>
+  </BrowserRouter>
   );
 }
 
