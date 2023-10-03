e4b4f5488f6ecce975c9f5e3f475fcb286794b49
Francisco Ramirez <96508706+FrancoRamirezz@users.noreply.github.com>
2023-10-02 17:19:54 -0700

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,25 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./App.css"
 function Navabr(){
+const Nav_logo = "https://www.calpolymissa.org/img/Missa+Logo.png";
+const Shop_Image = "https://emojipedia.org/shopping-cart";
+//const Shop_Image = "https://www.flaticon.com/free-icon/shopping-cart_263142"
 return < nav className="nav">
-<img src = " "/>
+<h1>Ecomerce</h1>
+<div>
+<img src = {Nav_logo}/>
 <a href = '/' className="site-title" >
-    Homepage
+    Landing Page
 </a>
+
 <ul>
-    <li>
-        <a href= "/shoppingcart" >ShoppingCart </a></li>
+    <li className="Active">
+        <Link to= "/shoppingcart" >ShoppingCart </Link></li>
         
     <li>
-    <a href= "/contact"> Contact</a>    
+    <Link to= "/contact"> Contact</Link>    
     </li>
     <li> 
-    <a href = "/About"> About</a>
+    <Link to= "/About"> About</Link>
     </li>
+    <nav>
+       <Link to ="/Homepage" >Link</Link> 
+    </nav>
 </ul>
 
 
-
+</div>
 </nav>
+
+
+
+}
+export default Navabr;
+
 
 
 }
