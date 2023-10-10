32da8dad2e7f241d8aab5244a3d3561f458fd8e4
Francisco Ramirez <96508706+FrancoRamirezz@users.noreply.github.com>
2023-10-10 03:32:47 -0700

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,25 @@
+import { Link } from "react-router-dom";
+import "./App.css";
+
 const Contact = () =>{
+
+
 return(
 <div>
 
-<h1> Contact page</h1>
-<p>   </p>
+        <h1>Contact page</h1>
+        <h2> Here is the follwoing link for my github </h2 >
+        
+        <ul>
+       <li>
+
+            Franco's Github is here
+            <nav a herf= "https://github.com/FrancoRamirezz"> Click the followinglink </nav>
+        </li >
+       
+
+
+        </ul>
 
 
 
@@ -12,3 +28,4 @@ return(
 
 );
 };
+export default Contact;
