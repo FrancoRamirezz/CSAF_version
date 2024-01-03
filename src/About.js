c223f04e94d71bc9e3996c1ef23794d7494435fd
Francisco Ramirez <96508706+FrancoRamirezz@users.noreply.github.com>
2024-01-02 23:18:53 -0800

    Update About.js

--- src/About.js
+++ src/About.js
@@ -6,7 +6,7 @@ return(
     <h1>About page</h1>
     <p>This Project is to demonstrate the importance of security measures when visiting an ecommerce website. This project consisted of
 different specialties, such software engineering, cybersecurity, and data analytics. This project could not have been done without the help
-from the CSAF lead Josh, the software team that consisted of Franco, Meghan, Ally, the data analytics team that consisted of Abhishek, and cyber/cloud
+from the CSAF lead Josh, the software team that consisted of Franco, Meghan, Ally, the data analytics team consisted of Abhishek, and cyber/cloud
 team that consisted of Josh, Palmer, and Salvador
 
     </p>
