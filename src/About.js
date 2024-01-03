f5130d6c322aed69c1a10ae2e6746a7d49608d2d
Francisco Ramirez <96508706+FrancoRamirezz@users.noreply.github.com>
2024-01-02 23:45:04 -0800

    Update About.js

--- src/About.js
+++ src/About.js
@@ -4,9 +4,9 @@ const About = () =>{
 return(
 <div>
     <h1>About page</h1>
-    <p>This Project is to demonstrate the importance of security measures when visiting an ecommerce website. This project consisted of
-different specialties, such software engineering, cybersecurity, and data analytics. This project could not have been done without the help
-from the CSAF lead Josh, the software team that consisted of Franco, Meghan, Ally, the data analytics team consisted of Abhishek, and cyber/cloud
+    <p>This Project is to demonstrate the importance of security measures when visiting an e-commerce website. This project consisted of
+different specialties, such as software engineering, cybersecurity, and data analytics. This project could not have been done without the help
+from the CSAF lead Josh, the software team consisted of Franco, Meghan, Ally, the data analytics team consisted of Abhishek, and cyber/cloud
 team that consisted of Josh, Palmer, and Salvador
 
     </p>
