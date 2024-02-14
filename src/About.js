082f24fddd7cec7a7cee2da389981a697e9c5250
Franco <96508706+FrancoRamirezz@users.noreply.github.com>
2024-02-14 00:45:51 -0800

    Update About.js

--- src/About.js
+++ src/About.js
@@ -2,16 +2,35 @@ import React from "react";
 const About = () =>{
 
 return(
-<div>
-    <h1>About page</h1>
-    <p>This Project is to demonstrate the importance of security measures when visiting an ecommerce website. This project consisted of
-different specialties, such as software engineering, cybersecurity, and data analytics. This project could not have been done without the help
-from the CSAF lead Josh, the software team that consisted of Franco, Meghan, Ally, the data analytics team consisted of Abhishek, and cyber/cloud
-team that consisted of Josh, Palmer, and Salvador
 
-    </p>
-
-</div>
+          <Card style={{ maxWidth: 800, margin: '40px auto', padding: '20px' }}>
+            <CardContent>
+              <Grid container spacing={3} alignItems="center" justifyContent="center">
+                <Grid item xs={12} sm={6}>
+                  <Avatar
+                    alt="Aboutpage"
+                    src="/Users/franciscoramirez/Desktop/VirgoAI.SAAS/client/virgoai-app/public/Canabout.png" // Replace with your image path
+                    style={{ width: 150, height: 150, margin: 'auto' }}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Typography variant="h5" gutterBottom>
+                    VirgoAI BudBuddy
+                  </Typography>
+                  <Typography variant="body1">
+                   We wanted to teach other students the importance of CyberSercuity in E-commerce 
+                  </Typography>
+                </Grid>
+              </Grid>
+      
+              <Typography variant="h6" style={{ marginTop: 20 }}>
+                We started off as a college kids, who saw a problem and we wanted to find a solution to it
+              </Typography>
+              <Typography variant="body1">
+                Thanks from the entire Missa CSAF team
+              </Typography>
+            </CardContent>
+          </Card>
 
 
 
