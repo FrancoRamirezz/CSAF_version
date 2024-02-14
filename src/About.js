4ea0f018428c5f93a1729d7ac2f741e475dd9e4b
Franco <96508706+FrancoRamirezz@users.noreply.github.com>
2024-02-14 00:40:29 -0800

    Update About.js

--- src/About.js
+++ src/About.js
@@ -1,18 +1,35 @@
-import React from "react";
-const About = () =>{
+import { Card, CardContent, Typography, Avatar, Grid } from '@mui/material'
+function Aboutpage() {
+    return (
 
-return(
-<div>
-    <h1>About page</h1>
-    <p>This Project is to demonstrate the importance of security measures when visiting an ecommerce website. This project consisted of
-different specialties, such as software engineering, cybersecurity, and data analytics. This project could not have been done without the help
-from the CSAF lead Josh, the software team that consisted of Franco, Meghan, Ally, the data analytics team consisted of Abhishek, and cyber/cloud
-team that consisted of Josh, Palmer, and Salvador
-
-    </p>
-
-</div>
-
+          <Card style={{ maxWidth: 800, margin: '40px auto', padding: '20px' }}>
+            <CardContent>
+              <Grid container spacing={3} alignItems="center" justifyContent="center">
+                <Grid item xs={12} sm={6}>
+                  <Avatar
+                    alt="Aboutpage"
+                    src="/Users/franciscoramirez/Desktop/VirgoAI.SAAS/client/virgoai-app/public/Canabout.png" // Replace with your image path
+                    style={{ width: 150, height: 150, margin: 'auto' }}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Typography variant="h5" gutterBottom>
+                    VirgoAI BudBuddy
+                  </Typography>
+                  <Typography variant="body1">
+                   CSAF mission to help other students learn the importance of cybersecurity
+                  </Typography>
+                </Grid>
+              </Grid>
+      
+              <Typography variant="h6" style={{ marginTop: 20 }}>
+                We started off as college kids, tasked with building an E Commerce project
+              </Typography>
+              <Typography variant="body1">
+               
+              </Typography>
+            </CardContent>
+          </Card>
 
 
 );
